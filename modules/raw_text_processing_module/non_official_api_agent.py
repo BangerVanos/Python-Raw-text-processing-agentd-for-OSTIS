025faea275c3cee5eb66f9011d1a8c9022c237dd
BangerVanos <romchik349@gmail.com>
2023-11-20 16:53:57 +0300

    Some changes + execute_agent test

--- modules/raw_text_processing_module/non_official_api_agent.py
+++ modules/raw_text_processing_module/non_official_api_agent.py
@@ -24,16 +24,19 @@ from sc_kpm import ScAgentClassic
 from . import raw_text_processing_configs as cf
 from .interfaces import IGetCleanText
 from requests import HTTPError
+import logging
 
 
-
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(levelname)s | %(name)s | %(message)s", datefmt="[%d-%b-%y %H:%M:%S]"
+)
 
 
 class NonOfficialAPITextProcessor(ScAgentClassic, IGetCleanText):
     def __init__(self) -> None:                     
-        super().__init__(cf.NON_OFFICIAL_API_AGENT_ACTION)        
+        super().__init__(cf.NON_OFFICIAL_API_AGENT_ACTION)               
 
-    def on_event(self, event_element: ScAddr, event_edge: ScAddr, action_element: ScAddr) -> ScResult:        
+    def on_event(self, event_element: ScAddr, event_edge: ScAddr, action_element: ScAddr) -> ScResult:                       
         self.logger.info('Non-official API raw text processor began to run...')        
         raw_text_node = get_action_arguments(action_element, 1)[0]
         if not raw_text_node:
@@ -44,25 +47,28 @@ class NonOfficialAPITextProcessor(ScAgentClassic, IGetCleanText):
             self.logger.error(f'Error: your raw text must be string type, but text of yours is {type(raw_text)}')
             return ScResult.ERROR_INVALID_TYPE
         try:
-            answer_link = create_link(self._get_clean_text(raw_text, 'lang_en'), ScLinkContentType.STRING)
+            clean_text = self._get_clean_text(raw_text, 'lang_en')
+            answer_link = create_link(clean_text, ScLinkContentType.STRING)
         except HTTPError as err:
             self.logger.error(f'Error: {err}.\nThis error is on non-official API\'s side.')
             return ScResult.ERROR
+        print(clean_text)
         create_action_answer(action_element, answer_link)
         self.logger.info('Successfully processed the text using non-official API! Wery well!')
         finish_action_with_status(action_element, True)
         return ScResult.OK
     
-    def _get_clean_text(raw_text: str, language: str) -> str:
+    def _get_clean_text(self, raw_text: str, language: str) -> str:
         """Method applies raw text describing structure from KB and language, which is string
         containing text like lang_YOUR_LANGUAGE_CODE (which is different representation for languages in
         OSTIS KBs). For example, english language will be lang_en. For text processing non-official
         LLM api is used. You can configure used model in raw_text_processing_configs file"""
 
         messages = [{'role': 'user', 'content': cf.PROMPTS[language].format(raw_text)}]
-        response = g4f.ChatCompletion(
+        response = g4f.ChatCompletion.create(
             model=cf.NON_OFFICIAL_API_DEFAULT_MODEL,
             messages=messages,
             temperature=0
         )
+        self.logger.info(f'Successfully cleaned text for you\n:{response}')
         return response    
--- server.py
+++ server.py
@@ -3,6 +3,15 @@ from sc_kpm import ScServer
 from modules.raw_text_processing_module.raw_text_processing_module import RawTextProcessingModule
 from modules.raw_text_processing_module import raw_text_processing_configs as cf
 
+from sc_kpm.utils import create_link, get_link_content_data
+from sc_kpm.utils.action_utils import (
+    create_action_answer,
+    execute_agent,
+    finish_action_with_status,
+    get_action_answer,
+    get_action_arguments,
+)
+import logging
 
 SC_SERVER_PROTOCOL = "protocol"
 SC_SERVER_HOST = "host"
@@ -12,18 +21,29 @@ SC_SERVER_PROTOCOL_DEFAULT = "ws"
 SC_SERVER_HOST_DEFAULT = "localhost"
 SC_SERVER_PORT_DEFAULT = "8090"
 
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(levelname)s | %(name)s | %(message)s", datefmt="[%d-%b-%y %H:%M:%S]"
+)
+
 
 def main(args: dict):
     server = ScServer(
-        f"{args[SC_SERVER_PROTOCOL]}://{args[SC_SERVER_HOST]}:{args[SC_SERVER_PORT]}")
+        f"{args[SC_SERVER_PROTOCOL]}://{args[SC_SERVER_HOST]}:{args[SC_SERVER_PORT]}/ws_json")
 
-    with server.connect():
+    with server.connect():        
         modules = [
             RawTextProcessingModule(),
         ]
-        server.add_modules(*modules)
+        server.add_modules(modules[0])
         with server.register_modules():
-            server.serve()
+            # question, is_successful = execute_agent(
+            # arguments={
+            #     create_link('apple color red; apple liked by human; apple place of growth tree', ScLinkContentType.STRING): False,                
+            # },
+            # concepts=[CommonIdentifiers.QUESTION, cf.NON_OFFICIAL_API_AGENT_ACTION],
+            # wait_time=1,
+            # )            
+            server.serve()    
 
 
 if __name__ == '__main__':
